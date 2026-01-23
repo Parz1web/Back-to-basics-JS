26a5d3cd4ce90e25d0aed750d88f1ed49897d5c9
Ivan <cool.vanka.sekirin@gmail.com>
2026-01-23 22:26:43 +0300

    Little snippet of code with example of global and local scopes

---
+++ 19-variable-scope/script.js
@@ -0,0 +1,21 @@
+let x = 1;
+// {
+//   let x = 2;
+// }
+
+let y = 3;
+
+firstFunc();
+secondFunc();
+
+function firstFunc() {
+  let x = 1;
+  let y = 10;
+  console.log(x);
+  console.log(y);
+}
+
+function secondFunc() {
+  let x = 2;
+  console.log(x);
+}
